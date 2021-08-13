f61a50ad6c9024b7b006b89db967a3033e0f903e
natasharrison <natasharrison@gmail.com>
2021-08-13 10:14:12 -0700

    second commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,90 @@
+// import user model
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const bookSchema = require('../models/Book');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+
+
+
+const resolvers = {
+
+  Query: {
+// get a single user by username
+  user: async(parent, {username}) => {
+      return User.findOne({username})
+  },
+//    get a single user by user id
+ me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+    },
+},
+
+Mutation: {
+
+createUser: async (parent, args) => {
+    const user = await User.create(args);
+    const token = signToken(user);
+
+    return { token, user };
+},
+
+login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new AuthenticationError('Unable to find user');
+    }
+
+    const correctPw = await user.isCorrectPassword(password);
+
+    if (!correctPw) {
+        throw new AuthenticationError('Incorrect password');
+    }
+
+    const token = signToken(user);
+    return { token, user };
+}, 
+
+  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+  // user comes from `req.user` created in the auth middleware function
+saveBook: async (parent, args, context) => {
+    if (context.user) {
+        const savedBooks = await bookSchema.create({ ...args, username: context.user.username});
+
+        await User.findByIdandUpdate(
+            {_id: context.user._id},
+            { $push: { thoughts: thought._id } },
+            { new: true }
+        );
+
+        return savedBooks;
+    }
+
+    throw new AuthenticationError('You need to be logged in!');
+}, 
+// remove a book from `savedBooks`
+deleteBook: async (parent, args, contect)
+
+
+
+
+module.exports = resolvers;
+
+
+  
+  async deleteBook({ user, params }, res) {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $pull: { savedBooks: { bookId: params.bookId } } },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Couldn't find user with this id!" });
+    }
+    return res.json(updatedUser);
+  },
+
